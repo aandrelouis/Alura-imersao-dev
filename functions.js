4c2e5e1063d547138b03252edee931a2012b78ed
aandrelouis <aandrelouis@gmail.com>
2022-09-12 11:29:21 -0300

    meio da aula

--- functions.js
+++ functions.js
@@ -1,61 +1,59 @@
-var nomeFilmes = [];
-
-function adicionarFilme(){
-    var filmeFavorito = document.getElementById("filme").value;
-    var nomeFilme = document.getElementById("filmeNome").value;
-
-    if (filmeFavorito.endsWith(".jpg")){   
-        nomeFilmes.push(nomeFilme);
-        listarFilmesNaTela(filmeFavorito);
+var carta1 = {
+    nome: "Bulbassauro",
+    imagem: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/001.png",
+    atributos: {
+        ataque: 7,
+        defesa: 8,
+        magia: 6
     }
-    else {
-        console.error("Endereço de imagem inválido");
-    }
-    
-    document.getElementById("filme").value = "";
+};
 
+var carta2 = {
+    nome: "Darth Vader",
+    imagem: "https://www.sideshowtoy.com/wp-content/uploads/2017/04/star-wars-episode-vi-return-of-the-jedi-darth-vader-sixth-scale-figure-hot-toys-feature-903269.jpg",
+    atributos: {
+        ataque: 9,
+        defesa: 8,
+        magia: 2
+    }
+ };
+
+var carta3 = {
+    nome: "Shiryu de Dragão",
+    imagem: "https://pm1.narvii.com/6319/1c3b3f4b3f9b1c3b3f4b3f9b1c3b3f4b3f9b1c3b_hq.jpg",
+    atributos: {
+        ataque: 5,
+        defesa: 9,
+        magia: 10
+    }
+};
+
+var cartaMaquina;
+var cartaJogador;
+var cartas = [carta1, carta2, carta3];
+
+function sortearCarta() {
+    var numeroCartaMaquina = parseInt(Math.random() * 3);
+    cartaMaquina = cartas[numeroCartaMaquina];
+
+    var numeroCartaJogador = parseInt(Math.random() * 3);
+    while (numeroCartaMaquina == numeroCartaJogador) {
+        numeroCartaJogador = parseInt(Math.random() * 3);
+    }
+    cartaJogador = cartas[numeroCartaJogador];
+    console.log(cartaJogador);
+
+    document.getElementById('btnSortear').disabled = true;
+    document.getElementById('btnJogar').disabled = false; 
+
+    exibirOpcoes();
 }
 
-
-function listarFilmesNaTela(filme) {
-    var elementoFilmeFavorito = "<div>" + "<img src=" + filme + ">" + "<p>" + nomeFilmes[nomeFilmes.length - 1] + "</p>" + "</div>";
-    var elementoListaFilmes = document.getElementById("listaFilmes");
-    elementoListaFilmes.innerHTML = elementoListaFilmes.innerHTML + elementoFilmeFavorito;
-}
-
-
-///////Desafios 
-//um botão para remover um filme da lista -- Feito
-function removerFilme(nome){
-    var nomeFimeRemover = document.getElementById("filmeNomeRemove").value;
-    var indice = nomeFilmes.indexOf(nomeFimeRemover);
-
-    if (indice > -1) {
-        var elementoListaFilmes = document.getElementById("listaFilmes");
-        for (var i = 0; i < nomeFilmes.length; i++){
-            if (nomeFilmes[i] == nomeFimeRemover){
-                nomeFilmes.splice(i, 1);
-                //remove um determinado elemento, nesse caso a div
-                elementoListaFilmes.removeChild(elementoListaFilmes.childNodes[i]);
-            }
-        }
+function exibirOpcoes() {
+    var opcoes = document.getElementById('opcoes');
+    var opcoesTexto = "";
+    for (var atributo in cartaJogador.atributos) {
+        opcoesTexto += "<input type='radio' name='atributo' value='" + atributo + "'>" + atributo;
     }
-    else {
-        console.error("Filme não encontrado");
-    }
-
-    
- 
-    console.log(elementoListaFilmes);
-
+    opcoes.innerHTML = opcoesTexto;
 }
-
-// colocar também o nome do filme e não só a imagem --Feito
-// também  guardar os filmes em uma lista e mostrar a lista de filmes na tela -- Feito
-// var nomeFilmes = [];
-
-
-
-// criar várias funções de conversão 
-// -- toda vez que chamar a função e passar parametro, devolver um valor(retorno)
-// -- colocar radio button para escolher qual conversão fazer
